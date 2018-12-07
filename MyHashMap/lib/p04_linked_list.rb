6369aa20bb2c022da265fb3270d1042c32a4ee17
Greg Waite <waite68@gmail.com>
2018-12-07 15:40:29 -0500

    Finish Linklist

--- MyHashMap/lib/p04_linked_list.rb
+++ MyHashMap/lib/p04_linked_list.rb
@@ -20,7 +20,13 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,34 +35,71 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    each { |node| return node.val if node.key == key }
   end
 
   def include?(key)
+    each do |node|
+      if node.key == key
+        return true
+      end
+    end 
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    second_to_last = last 
+
+    second_to_last.next = new_node
+    new_node.prev = second_to_last
+    new_node.next = @tail
+    @tail.prev = new_node 
   end
 
   def update(key, val)
+    each do |node|
+      if node.key == key 
+        node.val = val
+      end
+    end
   end
 
   def remove(key)
+    each do |node| 
+      if node.key == key
+        previous = node.prev
+        next_node = node.next 
+
+        previous.next, next_node.prev = next_node, previous 
+        break
+      end
+    end
   end
 
-  def each
+  def each(&prc) 
+    curr = @head.next
+
+    until curr == @tail
+      prc.call(curr)
+      curr = curr.next 
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
