75a8096ca467d84a8162b1642e17064f4e600c3e
Greg Waite <waite68@gmail.com>
2018-12-07 14:32:10 -0500

    Finish hash_set

--- MyHashMap/lib/p03_hash_set.rb
+++ MyHashMap/lib/p03_hash_set.rb
@@ -7,18 +7,27 @@ class HashSet
   end
 
   def insert(key)
+    resize! if @count >= num_buckets
+    self[key.hash] << key
+    @count += 1
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    if self.include?(key)
+      self[key.hash].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +35,9 @@ class HashSet
   end
 
   def resize!
+    @count = 0
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new } 
+    old_store.flatten.each { |el|  insert(el) } 
   end
 end
